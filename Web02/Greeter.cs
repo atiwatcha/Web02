6b358245c537d802cc83fc5787431a801bcd1153
admin <atiwat.chananet@gmail.com>
2017-12-17 13:50:39 +0700

    Add project files.

---
+++ Web02/Greeter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web02
+{
+    public class Greeter : IGreeter
+    {
+        public Greeter(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public string GetGreetingText()
+        {
+            return $"Hello say '{Configuration["greeting:en"]}'";
+        }
+    }
+}
